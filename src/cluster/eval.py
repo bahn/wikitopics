a087170a47929224cc5b656d1dc230e793c1f8ed
bahn <byunggyu.ahn@gmail.com>
2010-10-12 16:23:07 -0400

    fixed some bugs in cluster evuation scripts.

--- src/cluster/eval.py
+++ src/cluster/eval.py
@@ -11,11 +11,11 @@ clustering = "/Users/bahn/work/wikitopics/data/clustering/clusters-bahn/cluster0
 
 if sys.argv > 1:
     if len(sys.argv) == 3:
-	gold_file = sys.argv[1]
-	clustering = sys.argv[2]
+        gold_file = sys.argv[1]
+        clustering = sys.argv[2]
     else:
-	print 'Invalid arguments:', sys.argv
-	sys.exit(1)
+        print 'Invalid arguments:', sys.argv
+        sys.exit(1)
 
 label = {}
 cluster = {}
@@ -25,22 +25,22 @@ def read_clusters(filename, bag):
     numClusters = 0
     numInstances = 0
     for line in f:
-	if (line.find('#') != -1):
-	    line = line[:line.find('#')].strip()
-	tokens = line.split(None, 1)
-	if len(tokens) == 0:
-	    if numInstances:
-		numClusters += 1
-		numInstances = 0
-	else:
-	    topic = tokens[0].strip()
-	    if not topic in bag:
-		bag[topic] = set()
-	    bag[topic].add(numClusters)
-	    numInstances += 1
+        if (line.find('#') != -1): # eliminates comments
+            line = line[:line.find('#')].strip()
+        tokens = line.split(None, 1)
+        if len(tokens) == 0:
+            if numInstances:
+                numClusters += 1
+                numInstances = 0
+        else:
+            topic = tokens[0].strip()
+            if not topic in bag:
+                bag[topic] = set()
+            bag[topic].add(numClusters)
+            numInstances += 1
     if numInstances:
-	numClusters += 1
-	numInstances = 0
+        numClusters += 1
+        numInstances = 0
     return numClusters
     
 print "gold standard:", gold_file
@@ -54,13 +54,13 @@ del ret
 keys = label.keys()
 for key in keys:
     if not key in cluster:
-	print key, 'is missing in cluster'
-	sys.exit(0)
+        print key, 'is missing in cluster'
+        sys.exit(0)
 
 for key in cluster.keys():
     if not key in label:
-	print key, 'is missing in label'
-	sys.exit(0)
+        print key, 'is missing in label'
+        sys.exit(0)
 
 precision = 0.0
 recall = 0.0
@@ -73,23 +73,23 @@ for i, e in enumerate(keys):
     nomulprec = 0
     nomulrec = 0
     for i2, e2 in enumerate(keys):
-	if i == i2:
-	    continue
-	labelintersection = len(label[e] & label[e2])
-	clusterintersection = len(cluster[e] & cluster[e2])
-	less = min(labelintersection, clusterintersection)
-	if clusterintersection > 0:
-	    mulprec += float(less) / float(clusterintersection)
-	    nomulprec += 1
-	if labelintersection > 0:
-	    mulrec += float(less) / float(labelintersection)
-	    nomulrec += 1
+        if i == i2:
+            continue
+        labelintersection = len(label[e] & label[e2])
+        clusterintersection = len(cluster[e] & cluster[e2])
+        less = min(labelintersection, clusterintersection)
+        if clusterintersection > 0:
+            mulprec += float(less) / float(clusterintersection)
+            nomulprec += 1
+        if labelintersection > 0:
+            mulrec += float(less) / float(labelintersection)
+            nomulrec += 1
     if nomulprec > 0:
-	precision += mulprec / float(nomulprec)
-	noprecision += 1
+        precision += mulprec / float(nomulprec)
+        noprecision += 1
     if nomulrec > 0:
-	recall += mulrec / float(nomulrec)
-	norecall += 1
+        recall += mulrec / float(nomulrec)
+        norecall += 1
 if noprecision > 0:
     precision /= float(noprecision)
 if norecall > 0:
--- src/cluster/tabularize.perl
+++ src/cluster/tabularize.perl
@@ -7,15 +7,25 @@ while (<>) {
     chomp;
     $line = $_;
     if (/^gold standard/) {
+	# e.g. clusters-bahn/cluster0127.txt
+	# clusters-ben/pick0127
+	# clusters-ccb/pick0127.clusters-ccb
 	/clusters-([^\/]+)\/.+(\d{4})/;
 	$gold = $1;
 	$date = $2;
     }
     if (/^clustering/) {
+	# e.g. clusters-bahn/cluster0127.txt
+	# clusters-ben/pick0127
+	# clusters-ccb/pick0127.clusters-ccb
 	/clusters-([^\/]+)\/.+(\d{4})/;
 	$algo = $1;
 	$confirm_date = $2;
 	die if ($date ne $confirm_date);
+	# for cluster threshold: e.g. cluster-mturk.1
+	if (/clusters-mturk.(\d)/) {
+	    $algo = $algo . $1;
+	}
     }
     if (/BCubed/) {
 	if (/precision/) {
