c0921ffe93d146adaab84ab1bb14be49f583a297
bahn <byunggyu.ahn@gmail.com>
2010-11-29 14:54:19 -0500

    fixed unicode filename processing

--- src/mallet/ClusterFiles/ClusterFiles.java
+++ src/mallet/ClusterFiles/ClusterFiles.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.io.PrintStream;
 import java.util.*;
 import java.util.regex.*;
 
@@ -14,7 +16,40 @@ import cc.mallet.pipe.*;
 import cc.mallet.pipe.iterator.FileIterator;
 
 public class ClusterFiles {
+	static public String getPlainName(Instance instance) {
+		Pattern pattern = Pattern.compile("\\d+_\\d+_\\d+_\\d+_(.+)");
+
+		String name = instance.getName().toString();
+		if (name.startsWith("file:")) {
+			// trim the beginning substring "file:"
+			File file = new File(name.substring(5));
+			
+			// take only the name part. remove the path.
+			name = file.getName();
+			
+			// trim the trailing extension
+			if (name.endsWith(".txt")) {
+				name = name.substring(0, name.length() - 4);
+			}
+			if (name.endsWith(".sentences")) {
+				name = name.substring(0, name.length() - 10);
+			}
+			
+			// trim the starting date and revid
+			Matcher matcher = pattern.matcher(name);
+			if (matcher.find() && matcher.groupCount() == 1) {
+				name = matcher.group(1);
+			}
+		}
+
+		return name;
+	}
+
 	static public void main(String[] args) {
+		try {
+			System.setOut(new PrintStream(System.out, false, "UTF8"));
+		} catch (UnsupportedEncodingException e) {
+		}
 		if (args.length != 1 && args.length != 2) {
 			System.err.println("Usage: ClusterFiles [/path/to/dir/including/text/files/] [# of clusters]");
 			System.exit(1);
@@ -48,8 +83,6 @@ public class ClusterFiles {
 		Clustering clustering = kmeans.cluster(instances);
 		InstanceList[] clusters = clustering.getClusters();
 		
-		Pattern pattern = Pattern.compile("\\d+_\\d+_\\d+_\\d+_(.+)");
-		
 		//int clusterIndex = 0;
 		for (InstanceList cluster: clusters) {
 			//clusterIndex++;
@@ -65,41 +98,17 @@ public class ClusterFiles {
 		  			center = instance;
 		  		}
 		  	}
+			if (center != null) {
+				System.out.println(getPlainName(center));
+			}
 		  	
-			System.out.println("");
 			for (Instance instance: cluster) {
-				String name = instance.getName().toString();
-				if (name.startsWith("file:")) {
-					// trim the beginning substring "file:"
-					File file = new File(name.substring(5));
-					
-					// take only the name part. remove the path.
-					name = file.getName();
-					
-					// trim the trailing extension
-					if (name.endsWith(".txt")) {
-						name = name.substring(0, name.length() - 4);
-					}
-					if (name.endsWith(".sentences")) {
-						name = name.substring(0, name.length() - 10);
-					}
-					
-					// trim the starting date and revid
-					Matcher matcher = pattern.matcher(name);
-					if (matcher.find() && matcher.groupCount() == 1) {
-						name = matcher.group(1);
-					}
-
-					
-					if (center == instance) {
-						System.out.println(name);
-					} else {
-						System.out.println(name);
-					}
+				if (instance != center) {
+					System.out.println(getPlainName(instance));
 				}
 			}
+
+			System.out.println("");
 		}
-		
-		
 	}
 }
