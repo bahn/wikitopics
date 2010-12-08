25043dfdee4a60f06e7128a2649d9012f7b91f1f
bahn <byunggyu.ahn@gmail.com>
2010-12-08 04:57:32 -0500

    added cluster scripts and evaluation results

---
+++ src/cluster/batch_eval_mturk.sh
@@ -0,0 +1,11 @@
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ccb/pick0127.clusters-ccb /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.1 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ccb/pick0127.clusters-ccb /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.2 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ccb/pick0127.clusters-ccb /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.3 
+
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ben/pick0127 /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.1 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ben/pick0127 /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.2 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ben/pick0127 /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.3 
+
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-bahn/cluster0127.txt /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.1 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-bahn/cluster0127.txt /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.2 
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-bahn/cluster0127.txt /Users/bahn/work/wikitopics/data/clustering/clusters-mturk/pick0127.clusters-mturk.3 
---
+++ src/cluster/eval_mturk_mallet.sh
@@ -0,0 +1,3 @@
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ccb/pick0127.clusters-ccb /Users/bahn/work/wikitopics/data/clustering/clusters-mturk-mallet/pick0127.clusters-mturk-mallet.38
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-ben/pick0127 /Users/bahn/work/wikitopics/data/clustering/clusters-mturk-mallet/pick0127.clusters-mturk-mallet.38
+./eval.py /Users/bahn/work/wikitopics/data/clustering/clusters-bahn/cluster0127.txt /Users/bahn/work/wikitopics/data/clustering/clusters-mturk-mallet/pick0127.clusters-mturk-mallet.38
