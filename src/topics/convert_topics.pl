8e9a4b8dfdc8bddea9bf9a610e01a4840b2c74b2
bahn <byunggyu.ahn@gmail.com>
2011-03-30 23:51:48 -0400

    updated convert_topics

--- src/topics/convert_topics.pl
+++ src/topics/convert_topics.pl
@@ -1,10 +1,21 @@
 #!/usr/bin/env perl
+use URI::Escape;
+$la='en';
+if ($#ARGV >= 0 && $ARGV[0] eq '-l') {
+    $la=$ARGV[1];
+    shift; shift;
+}
+
 print "<table>\n";
 print "<tr><th>Rank</th><th>Titles and links</th><th>Trending score</th></tr>\n";
 while (<>) {
     /^([^ ]*) /;
-    $title = $1;
-    s| | <a href="http://en.wikipedia.org/wiki/$title" target="view">[now]</a></td><td>|;
+    $title = uri_escape($1);
+    if ($la eq "en") {
+        s| | <a href="http://$la.wikipedia.org/wiki/$title" target="view">[now]</a></td><td>|;
+    } else {
+        s| | <a href="http://$la.wikipedia.org/wiki/$title" target="view">[now]</a> <a href="http://translate.google.com/translate?hl=en&sl=$la&tl=en&u=http%3A%2F%2F$la.wikipedia.org%2Fwiki%2F$title" target="translate">[now:translate]</a></td><td>|;
+    }
     s|^|<tr><td>$.</td><td>|;
     s|$|</td></tr>|;
     print;
---
+++ src/topics/convert_topics.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import sys
+import re
+import datetime
+import urllib
+import os
+try:
+	import wikipydia
+except ImportError:
+	sys.path.append('/home/bahn/work/wikipydia')
+	import wikipydia
+
+def convert_topics(filename, lang):
+	date = None
+	topics_re = re.compile(r'^([0-9]{4})-([0-9]{2})-([0-9]{2})\.topics$')
+	m = topics_re.match(os.path.basename(filename))
+	if m:
+		date = datetime.date(int(m.group(1)), int(m.group(2)), int(m.group(3)))
+
+	lineno = 0
+	with open(filename, 'r') as f:
+		print "<table>";
+		print "<tr><th>Rank</th><th>Titles and links</th><th>Trending score</th></tr>";
+		for line in f:
+			lineno += 1
+			i = line.find(' ')
+			if i == -1:
+				i = line.find('\t')
+			if i == -1:
+				title = line.strip()
+				pageviews = None
+			else:
+				title = line[:i]
+				pageviews = int(line[i+1:])
+			if not wikipydia.query_exists(title, lang):
+				continue
+			title = wikipydia.query_redirects(title, lang)
+			if date:
+				oldid = str(wikipydia.query_revid_by_date(title, lang, date))
+			escaped_title = urllib.quote(title.replace(' ','_'))
+			print '<tr><td>%d</td><td>%s <a href="http://%s.wikipedia.org/wiki/%s" target="view">[now]</a>' % (lineno, title, lang, escaped_title),
+			if date:
+				print ' <a href="http://' + lang + '.wikipedia.org/w/index.php?title=' + escaped_title + '&oldid=' + oldid + '" target="viewthen">[then]</a>',
+			if lang != 'en':
+				print ' <a href="http://translate.google.com/translate?hl=en&sl=' + lang + '&tl=en&u=http%3A%2F%2F' + lang + '.wikipedia.org%2Fwiki%2F' + escaped_title + '" target="translate">[now:translate]</a>',
+				if date:
+					print ' <a href="http://translate.google.com/translate?hl=en&sl=' + lang + '&tl=en&u=http%3A%2F%2F' + lang + '.wikipedia.org%2Fw%2Findex.php?title=' + escaped_title + '&oldid=' + oldid + '" target="translatethen">[then:translate]</a>',
+			if pageviews:
+				print "</td><td>%d" % (pageviews),
+			print "</td></tr>";
+		print "</table>";
+
+if __name__=='__main__':
+	lang = 'en'
+	while len(sys.argv) > 1 and sys.argv[1].startswith('-'):
+		if len(sys.argv) > 2 and sys.argv[1] == '-l':
+			lang = sys.argv[2]
+			sys.argv[1:3] = []
+		else:
+			sys.stderr.write('Unknown switch: %s\n' % sys.argv[1])
+			sys.exit(1)
+	if len(sys.argv) != 2:
+		sys.stderr.write('Usage: %s [-l LANG] FILE\n' % sys.argv[0])
+		sys.exit(1)
+	filename = sys.argv[1]
+	convert_topics(filename, lang)
+
