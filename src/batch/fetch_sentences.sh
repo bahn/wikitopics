566ba7e9238360de59e7361b1e35a69a54123b36
Byung Gyu Ahn <bahn@cs.jhu.edu>
2011-04-06 21:27:19 -0400

    added the dry run feature to fetch_sentences.sh

--- src/batch/fetch_sentences.sh
+++ src/batch/fetch_sentences.sh
@@ -12,13 +12,17 @@ if [ ! -f "$SCRIPT" ]; then
 	exit 1
 fi
 
+if [ "$1" == "--dry-run" ]; then
+	DRYRUN=1
+	shift
+fi
 if [ "$1" == "-v" ]; then
 	VERBOSE=1
 	shift
 fi
 
 if [ $# -lt 2 -o $# -gt 3 ]; then
-	echo "USAGE: $0 LANGUAGE START_DATE [END_DATE]" >&2
+	echo "USAGE: $0 [--dry-run] [-v] LANGUAGE START_DATE [END_DATE]" >&2
 	exit 1
 fi
 
@@ -59,5 +63,9 @@ for FILE in $TOPIC_DIR/$LLANG/*/*; do
 	YEAR="${BASENAME:0:4}"
 	OUTPUT_DIR="$ARTICLE_DIR/$LLANG/$YEAR"
 	mkdir -p "$OUTPUT_DIR"
-	$SCRIPT -l $LLANG -d $BASENAME -o $OUTPUT_DIR $FILE
+	if [ $DRYRUN ]; then
+		echo "$SCRIPT -l $LLANG -d $BASENAME -o $OUTPUT_DIR $FILE"
+	else
+		$SCRIPT -l $LLANG -d $BASENAME -o $OUTPUT_DIR $FILE
+	fi
 done
