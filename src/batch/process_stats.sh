8f12024eb20ff4bafd1d021753e5fab1ba454c7f
Byung Gyu Ahn <bahn@cs.jhu.edu>
2011-04-11 20:32:10 -0400

    fixed the process_stats.sh script

--- src/batch/process_stats.sh
+++ src/batch/process_stats.sh
@@ -32,7 +32,7 @@ then
     exit 1
 fi
 
-DATASET="$1"
+DATA_SET="$1"
 START_DATE=`date --date "$2" +"%Y%m%d"`
 if [ "$3" == "" ]; then
 	END_DATE=$START_DATE
@@ -41,17 +41,16 @@ else
 fi
 
 # don't use LANG or LANGUAGE -- they are used by Perl.
-LANG_OPTION=`echo $DATASET | sed -e 's/-.\+$//'`
-if echo $DATASET | grep - > /dev/null; then
-	FILTER="-f `echo $DATASET | sed -e 's/^.\+-//'`"
+LANG_OPTION=`echo $DATA_SET | sed -e 's/-.\+$//'`
+if echo $DATA_SET | grep - > /dev/null; then
+	FILTER="-f `echo $DATA_SET | sed -e 's/^.\+-//'`"
 fi
 
-OUTPUT_DIR="$WIKISTATS/process/$DATASET"
+INPUT_DIR="$WIKISTATS/archive"
+OUTPUT_DIR="$WIKISTATS/process/$DATA_SET"
 
-echo process_stats: add_hourly_stats.sh -l $LANG_OPTION $FILTER archive $OUTPUT_DIR/daily $START_DATE $END_DATE >&2
-time $WIKITOPICS/src/batch/add_hourly_stats.sh -l $LA $WIKISTATS/archive $OUTPUT_DIR/daily $START_DATE $END_DATE
+time $WIKITOPICS/src/batch/add_hourly_stats.sh -l $LANG_OPTION $INPUT_DIR $OUTPUT_DIR/daily $START_DATE $END_DATE
 
 if [ "$REDIRECTS" != "" ]; then
-    echo process_stats: redirect_stats.sh $LANG_OPTION $REDIRECTS $OUTPUT_DIR/daily $OUTPUT_DIR/redir/daily $START_DATE $END_DATE >&2
     time $WIKITOPICS/src/batch/redirect_stats.sh $LANG_OPTION $REDIRECTS $OUTPUT_DIR/daily $OUTPUT_DIR/redir/daily $START_DATE $END_DATE
 fi
