554bb41ac8b5f1859fd1518aa5da4a763b646f91
Byung Gyu Ahn <bahn@cs.jhu.edu>
2011-04-13 22:06:41 -0400

    process_stats.sh does all and doesn't need redirects to be specified.

--- src/batch/process_stats.sh
+++ src/batch/process_stats.sh
@@ -27,7 +27,7 @@ done
 
 if [ $# -lt 2 -o $# -gt 3 ]
 then
-    echo "Usage: $0 [-r REDIRECTS] DATA_SET START_DATE [END_DATE]" >&2
+    echo "Usage: $0 DATA_SET START_DATE [END_DATE]" >&2
     echo "Given command-line options: $*" >&2
     exit 1
 fi
@@ -48,9 +48,22 @@ fi
 
 INPUT_DIR="$WIKISTATS/archive"
 OUTPUT_DIR="$WIKISTATS/process/$DATA_SET"
+if [ "$DATA_SET" == "ko" ]; then
+	REDIRECTS="$WIKIDUMP/kowiki-20110303/redirects.txt"
+elif [ "$DATA_SET" == "ja" ]; then
+	REDIRECTS="$WIKIDUMP/jawiki-20110308/redirects.txt"
+	CUT_OFF="-c 100"
+elif [ "$DATA_SET" == "en" ]; then
+	REDIRECTS="$WIKIDUMP/enwiki-20110115/redirects.txt"
+	CUT_OFF="-c 100"
+elif [ "$DATA_SET" == "en-10" ]; then
+	REDIRECTS="$WIKIDUMP/enwiki-20110115/redirects.txt"
+fi
 
 time $WIKITOPICS/src/batch/add_hourly_stats.sh $DATA_SET $START_DATE $END_DATE
 
 if [ "$REDIRECTS" != "" ]; then
     time $WIKITOPICS/src/batch/redirect_stats.sh $DATA_SET $REDIRECTS $START_DATE $END_DATE
 fi
+
+time $WIKITOPICS/src/batch/list_topics.sh $CUT_OFF $DATA_SET $START_DATE $END_DATE
